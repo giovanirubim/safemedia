77dfc79dbf06d51f0254f6de1c6600b1323b6d27
Giovani Rubim <giovanirubim.f@gmail.com>
2020-05-14 22:17:19 -0300

    ainda nao consegui por imagens

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
-const { app, BrowserWindow, ipcMain } = require('electron');
 
+const {
+	app,
+	BrowserWindow,
+	ipcMain,
+	nativeImage
+} = require('electron');
+
+const history = [];
 const context = {
 	pwd: __dirname.replace(/\\/g, '/').replace(/\/$/, '') + '/',
 	files: []
@@ -12,27 +19,56 @@ const updateContext = () => {
 	const names = fs.readdirSync(context.pwd);
 	names.forEach(name => {
 		const path = pwd + name;
-		const stat = fs.lstatSync(path);
-		const isDir = stat.isDirectory();
-		const size = stat.size;
-		files.push({ name, isDir, size });
+		try {
+			const stat = fs.lstatSync(path);
+			const size = stat.size;
+			const isDir = stat.isDirectory();
+			const key = (isDir^1) + name.toUpperCase();
+			files.push({ name, isDir, size, key });
+		} catch(err) {
+			return;
+		}
 	});
+	files.sort((a, b) => a.key > b.key? 1: -1);
 };
 
 const createMainWindow = () => {
 	let win = new BrowserWindow({
 		width: 800,
 		height: 600,
-		webPreferences: { nodeIntegration: true }
+		webPreferences: { nodeIntegration: true },
+		icon: './icon.png'
 	});
 	win.removeMenu();
 	win.loadFile('./web/index.html');
 	win.webContents.openDevTools();
 };
 
-ipcMain.on('ctxreq', e => {
+ipcMain.on('ctx-req', e => {
 	updateContext();
-	e.reply('ctxres', context);
+	e.reply('ctx-update', context);
+});
+ipcMain.on('enter', (e, file) => {
+	history.push(context.pwd);
+	context.pwd += file.name + '/';
+	updateContext();
+	e.reply('ctx-update', context);
+});
+ipcMain.on('back', (e, file) => {
+	if (!history.length) return;
+	context.pwd = history.splice(history.length - 1, 1)[0];
+	updateContext();
+	e.reply('ctx-update', context);
+});
+ipcMain.on('up', (e, file) => {
+	let { pwd } = context;
+	if (!pwd.includes('/')) return;
+	history.push(pwd);
+	console.log(pwd);
+	context.pwd = pwd.replace(/\/[^\/]+\/$/, '/');
+	console.log(context.pwd);
+	updateContext();
+	e.reply('ctx-update', context);
 });
 
 app.whenReady().then(createMainWindow);
--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,58 @@
 const { ipcRenderer } = require('electron');
+const $ = require('jQuery');
 
-ipcRenderer.on('ctxres', (e, ctx) => {
-	console.log(ctx);
+const $str = (str) => $(document.createTextNode(str));
+
+let fileMap = {};
+let lastId = 0;
+
+let theme = 'dark';
+const imgMap = {
+	file: '/img/file-%theme%.png',
+	folder: '/img/folder-%theme%.png',
+};
+
+const fileToDOM = (file) => {
+
+	const id = 'file_' + (++lastId);
+	fileMap[id] = { ...file };
+
+	const dom = $(document.createElement('div'));
+	if (file.isDir) dom.html('&#128193;');
+	dom.attr({ id });
+	dom.addClass('file-item');
+	if (file.isDir) dom.addClass('directory');
+	dom.append($str(file.name+(file.isDir?'/':'')));
+	return dom;
+
+};
+
+const updateCtx = (ctx) => {
+	const body = $('body').html('');
+	lastId = 0;
+	fileMap = {};
+	ctx.files.forEach((file) => body.append(fileToDOM(file)));
+};
+
+const bindKeys = () => {
+	$(window).bind('keydown', e => {
+		const key = e.key.trim().toLowerCase().replace('arrow', '');
+		if (key === 'up' && e.altKey && !e.ctrlKey && !e.shiftKey) {
+			ipcRenderer.send('up');
+		}
+		if (key === 'left' && e.altKey && !e.ctrlKey && !e.shiftKey) {
+			ipcRenderer.send('back');
+		}
+	});
+};
+
+$(document).ready(() => {
+	ipcRenderer.on('ctx-update', (e, ctx) => updateCtx(ctx));
+	ipcRenderer.send('ctx-req');
+	$('body').on('dblclick', '.file-item.directory', function(){
+		const id = $(this).attr('id');
+		const file = fileMap[id];
+		ipcRenderer.send('enter', file);
+	});
+	bindKeys();
 });
-
-ipcRenderer.send('ctxreq');
